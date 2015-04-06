b33e4097b99870d098719cd89cb81f62fddc0cae
Brett Moore & bountham <student@epicodus.com>
2015-04-06 10:30:21 -0700

    add leapyear webpage and tests

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+var leapYear = function(year) {
+    if ((year % 4 === 0) && (year % 100 !== 0)) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+$(document).ready(function(){
+    $("form#leap-year").submit(function(event){
+        var year = parseInt($('input#year').val());
+        var result = leapYear(year);
+
+
+        $(".year").text(year);
+        $(".not").text("");
+        if (!result) {
+          $(".not").text("not");
+
+          }
+
+        $("#result").show();
+        event.preventDefault();
+
+    });
+});
